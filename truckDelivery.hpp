32fe38e80164cf84e14c708b94479d4f761babc1
Aqibkhan2023 <mk06181@st.habib.edu.pk>
2021-02-21 00:52:18 +0500

    Included maps

--- truckDelivery.hpp
+++ truckDelivery.hpp
@@ -1,11 +1,14 @@
 #include "truck.hpp"
 #include <vector>
+#include <map>
 using namespace std;
 
 class TruckDelivery{
     vector<Truck> trucks;
 public:
-    void loadTrucks();
+    void loadTrucks(){
+
+    }
     void calculateCost();
     void makeJourney();
     void unloadTrucks();
