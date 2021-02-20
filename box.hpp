c7a9eb9666a6d0daddbc4e0011dbd20d93202d5e
hsntrq <mt05491@st.habib.edu.pk>
2021-02-21 00:37:55 +0500

    added code to box

--- box.hpp
+++ box.hpp
@@ -1,14 +1,24 @@
 #pragma once
 #include<iostream>
+#include<random>
+#include<time.h>
+
 using namespace std;
 
-class Box
-{
-    int length;
-    int width;
-    int height;
-public:
-    Box();
-
-    int volume();
+class Box{
+    private:
+        int length;
+        int width;
+        int height;
+        int volume = length * width * height;
+    public:
+        Box(){
+            srand(time(0));
+            length = (rand() %25) + 5;
+            width = (rand() %25) + 5;
+            height = (rand() %25) + 5;
+        }
+        int volume(){
+            return volume;
+        }
 };
