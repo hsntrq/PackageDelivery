ec6c40d5a18be5c787064e9f0008430135864397
Aqibkhan2023 <mk06181@st.habib.edu.pk>
2021-02-21 03:52:59 +0500

    Cost updated

--- truck.hpp
+++ truck.hpp
@@ -6,13 +6,13 @@ class Truck
 {
     private:
         string driver;
-        int petrol;
+        float petrol;
         int money;
-        int fullMileage;
-        int emptyMileage;
+        float fullMileage;
+        float emptyMileage;
         Box box[10];
     public:
-        Truck(string drv, int petr, int mon, int fullm, int emptym){
+        Truck(string drv, float petr, int mon, float fullm, float emptym){
             driver = drv;
             petrol = petr;
             money = mon;
@@ -29,9 +29,9 @@ class Truck
         float cost(){
             float total_petrol = petrol + money  / 2.73;
             if (total_petrol > 50) total_petrol = 50;
-            float petrol_required = 60 / fullMileage + 60 / emptyMileage;
-            if (petrol_required < total_petrol){
-                if (petrol_required-petrol > 0)
+            float petrol_required = 60.0 / fullMileage + 60.0 / emptyMileage;
+            if (petrol_required <= total_petrol){
+                if (petrol_required-petrol >= 0)
                     return 2.73*(petrol_required-petrol);
                 else    
                     return 0;
