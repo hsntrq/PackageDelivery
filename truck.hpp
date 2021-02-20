cdb79268988a01bf4bbd274d2afacd4258e2822c
hsntrq <mt05491@st.habib.edu.pk>
2021-02-21 02:08:26 +0500

    added code in Truck class

--- truck.hpp
+++ truck.hpp
@@ -2,15 +2,28 @@
 #include "box.hpp"
 class Truck
 {
-    string driver;
-    int petrol;
-    int money;
-    int fullMileage;
-    int emptyMileage;
-    Box box[10];
+    private:
+        string driver;
+        int petrol;
+        int money;
+        int fullMileage;
+        int emptyMileage;
+        Box box[10];
+    public:
+        Truck(string drv, int petr, int mon, int fullm, int emptym){
+            driver = drv;
+            petrol = petr;
+            money = mon;
+            fullMileage = fullm;
+            emptyMileage = emptym;
+        }
 
-    void load(int numBox);
-    void unload();
-    float cost();
+        void load(){
+            for (int i = 0; i < 10; i++) cout<<box[i];
+        }
+        void unload(){
+            for (int i = 0; i < 10; i++) cout<<box[i];
+        }
+        float cost();
 };
 
